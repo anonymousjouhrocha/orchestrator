22289952a8ac07374d059b70c40f70428fddbcc1
anonymousjouhrocha <anonymous.jouhrocha2@gmail.com>
2025-09-02 10:19:53 +0000

    feat: v0.1.0 orchestrator multi-IA (Python)

--- tests/test_orchestrator.py
+++
@@ -1,6 +0,0 @@
-from src.core.orchestrator import Orchestrator, Challenge
-
-def test_dispatch():
-    orch = Orchestrator(agents=["gpt"])
-    res = orch.dispatch(Challenge(text="Say hi"))
-    assert "challenge_id" in res
